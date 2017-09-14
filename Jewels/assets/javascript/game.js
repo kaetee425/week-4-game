df5a5d871c2646d15fa918b864bcf1a2ba443393
dblanchard13 <davidb.prof@gmail.com>
2017-09-14 11:55:42 -0700

    Homework comments.

--- Jewels/assets/javascript/game.js
+++ Jewels/assets/javascript/game.js
@@ -1,3 +1,4 @@
+// Nice job putting all of your code in a document ready block 👌
 $(document).ready(function(){
 
 
@@ -10,40 +11,61 @@ $(document).ready(function(){
 	var wins = 0;
 	var losses = 0;
 
-	var imageOne = 0;
-	var imageTwo = 0;
-	var imageThree = 0;
-	var imageFour = 0;
-	var imageFive = 0;
+	// See my note below all of your listeners to see why 
+	// I've moved these variables into an object.
+	//
+	var crystalValues = {
+		imageOne: 0,
+		imageTwo: 0,
+		imageThree: 0,
+		imageFour: 0,
+		imageFive: 0
+	}
 
 	// $("#target").text(targetNum)
 	// $("#points").text(sum)
 
 	var targetNum = 0;
-	console.log(targetNum)
+
+	// console.log's are awesome for validating your work when developing a program
+	// but it's generally best practice to remove them from production code.
+	// console.log(targetNum)
 	var sum = 0;
 
+	// I really like how you split up your game logic into functions.
+	// It's a much easier style to understand 🤓
 	function startGame () {
-		targetNum = Math.floor(Math.random() * (120 - 19)) + 19;
+		// Since the string of code for generating random numbers
+		// is rather unwieldy I'd suggest placing it in a utility function
+		targetNum = getRandomNum(120, 19)
 		$("#target").text(targetNum)
 
-		imageOne = Math.floor(Math.random() * (12 - 1)) + 1
+		crystalValues['imageOne'] = getRandomNum(12, 1)
 		// console.log(imageOne)
-		imageTwo = Math.floor(Math.random() * (12 - 1)) + 1
+		crystalValues['imageTwo'] = getRandomNum(12, 1)
 		// console.log(imageTwo)
-		imageThree = Math.floor(Math.random() * (12 - 1)) + 1
+		crystalValues['imageThree'] = getRandomNum(12, 1)
 		// console.log(imageThree)
-		imageFour = Math.floor(Math.random() * (12 - 1)) + 1
+		crystalValues['imageFour'] = getRandomNum(12, 1)
 		// console.log(imageFour)
-		imageFive = Math.floor(Math.random() * (12 - 1)) + 1
+		crystalValues['imageFive'] = getRandomNum(12, 1)
 		// console.log(imageFive)
+		// it's not really necessary to use a return statement here since
+		// you're not actually using the return value from this function.
 		return sum = 0;
+	}
+
+	function getRandomNum (max, min) {
+		return Math.floor(Math.random() * (max - min) + min);
 	}
 
 	startGame();
 
 	function results () {
+		// nice job using triple equals for equality checking 🙌
 		if (sum === targetNum){
+			// So it's totally fine to end code statements with semi-colons or to leave them off
+			// just do your best to be consistent one way or the other 😬
 			wins += 1;
 			alert("WINNER!!!!!")
 			$("#wins").text(wins)
@@ -57,41 +79,36 @@ $(document).ready(function(){
 		}
 	}
 
-	$("#imageOne").on("click", function() {
-		sum += imageOne
-		results();
-		$("#points").text(sum)
-		console.log(imageOne)
-	})
 
-	$("#imageTwo").on("click", function() {
-		sum += imageTwo
-		results();
-		$("#points").text(sum)
-		console.log(imageTwo)
-	})
+	// Your  event listener code was pretty repetitive, so in an effort to DRY it up I'd suggest combining
+	// them all into 1 and then determining the value of the clicked crystal by checking its id.
+	// I moved all of the crystal values into an object for this so I could leverage bracket notation
+	// which will try to access the property that the expression inside of the brackets evaluates to.
+	// Take the following object:
+	// 
+	// var obj = { name: 'Katie' }
+	// 
+	// You could do the following to access the name property:
+	// 
+	// var firstHalf = 'na'
+	// var secondHalf = 'me'
+	// 
+	// obj[ firstHalf + secondHalf ] // would return 'Katie'
+	// 
 
-	$("#imageThree").on("click", function() {
-		sum += imageThree
-		results();
-		$("#points").text(sum)
-		console.log(imageThree)
-	})
+	$('.jewels').on('click', function() {
 
-	$("#imageFour").on("click", function() {
-		sum += imageFour
-		results();
-		$("#points").text(sum)
-		console.log(imageFour)
-	})
+		// grab the elements id
+		var crystalId = $(this).attr('id') 
 
-	$("#imageFive").on("click", function() {
-		sum += imageFive
-		results();
-		$("#points").text(sum)
-		console.log(imageFive)
-	})
+		// access the corresponding value using the id and add it to the sum
+		sum += crystalValues[ crystalId ]
 
+		results();
+
+		$("#points").text(sum)
+
+	})
 
 
 	// console.log(typeof(sum))
